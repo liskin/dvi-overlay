055d282280629885b42245aee006925c881469a8
Tomas Janousek <tomi@nomi.cz>
2011-06-19 16:28:53 +0200

    Synchronization of certain page breaks.

--- overlay.sh
+++ overlay.sh
@@ -11,12 +11,13 @@ if [ -z "$LATEX" ]; then
     LATEX=latex
 fi
 
-labels() {
+aux() {
     # The % at the end helps us distinguish between original and added entries.
     perl -ne 'if(/^(\\newlabel.*\{)(\S+\}\{\}\})$/){ print $1 . "'$2':" . $2 . "%\n" }' $1
+    perl -ne 'if(/^(.*overlay@sync.*\})$/){ print $1 . "%\n" }' $1
 }
 
 A1=${1%.tex}.aux
 A2=${2%.tex}.aux
 
-$LATEX $1 && $LATEX $2 && labels $A1 doc1 >>$A2 && labels $A2 doc2 >>$A1
+$LATEX $1 && $LATEX $2 && aux $A1 doc1 >>$A2 && aux $A2 doc2 >>$A1
