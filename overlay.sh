546b51dec8938a87a3353b7e57d9f40f204edbda
Tomas Janousek <tomi@nomi.cz>
2011-06-19 13:27:44 +0200

    Use LATEX instead of TEX in overlay.sh.

--- overlay.sh
+++ overlay.sh
@@ -7,8 +7,8 @@ if [ $# != 2 ]; then
     exit 3
 fi
 
-if [ -z "$TEX" ]; then
-    TEX=latex
+if [ -z "$LATEX" ]; then
+    LATEX=latex
 fi
 
 labels() {
@@ -19,4 +19,4 @@ labels() {
 A1=${1%.tex}.aux
 A2=${2%.tex}.aux
 
-$TEX $1 && $TEX $2 && labels $A1 doc1 >>$A2 && labels $A2 doc2 >>$A1
+$LATEX $1 && $LATEX $2 && labels $A1 doc1 >>$A2 && labels $A2 doc2 >>$A1
