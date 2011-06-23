c7b7e47bcf090fe903a794643edc1a1f0e63a9fb
Tomas Janousek <tomi@nomi.cz>
2011-06-23 18:24:43 +0200

    overlay.sh reruns latex automatically a few times

--- overlay.sh
+++ overlay.sh
@@ -17,12 +17,33 @@ aux() {
     perl -ne 'if(/^(.*overlay@sync.*\})$/){ print $1 . "%\n" }' $1
 }
 
+I1=$1
+I2=$2
 A1=${1%.tex}.aux
 A2=${2%.tex}.aux
+L1=${1%.tex}.log
+L2=${2%.tex}.log
+
+iter() {
+    $LATEX $I1
+    $LATEX $I2
+    aux $A1 doc1 >>$A2
+    aux $A2 doc2 >>$A1
+}
+
+pages() {
+    perl -ne 'if(/^Output written on .* \((\d+) pages,/){ print $1 }' $1
+}
+
+again() {
+    egrep 'Rerun (LaTeX|to get cross-references right|to get citations correct)' $L1 $L2 || [ "`pages $L1`" != "`pages $L2`" ]
+}
 
 set -e
 
-$LATEX $1
-$LATEX $2
-aux $A1 doc1 >>$A2
-aux $A2 doc2 >>$A1
+latex_count=20
+
+iter
+while again && [ $[latex_count--] -gt 0 ]; do
+    iter
+done
